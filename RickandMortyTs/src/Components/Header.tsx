f814264162b9df039955438441084cebbe0d2cf3
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-11 15:13:58 +0100

    Home

---
+++ RickandMortyTs/src/Components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <div className="h-40 bg-red-800 my-2 flex justify-center items-center ">
+        <h1 className="text-7xl font-bold">Rick and Morty </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ RickandMortyTs/src/Components/Nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <div className=" h-24 flex justify-end ">
+        <ul className="px-2 text-black flex h-12 items-center self-center text-xl">
+            <li className="mx-1">Characters</li>
+            <li className="mx-2">Episodes</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
