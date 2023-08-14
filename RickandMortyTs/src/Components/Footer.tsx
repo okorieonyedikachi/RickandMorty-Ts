b6dd4b10668ef1225aa732543c559c01e50aa542
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-14 18:38:21 +0100

    Media Query for mobile view

---
+++ RickandMortyTs/src/Components/Footer.tsx
@@ -0,0 +1,21 @@
+
+const Footer = () => {
+    function getCurrentYear(): number{
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        return currentYear
+    }
+    const currentYear = getCurrentYear()
+   
+  return (
+    <footer className="bg-zinc-900 h-max fixed w-full bottom-0">
+        <div>
+            <p className="text-zinc-100 flex justify-center py-4">
+                by Dannie {currentYear}
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
