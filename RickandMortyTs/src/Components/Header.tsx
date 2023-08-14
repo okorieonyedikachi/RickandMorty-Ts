0dc777bad796a6dfc457c2370edbcc8a043fae6d
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-14 18:38:43 +0100

    Media query for Text on header

--- RickandMortyTs/src/Components/Header.tsx
+++ RickandMortyTs/src/Components/Header.tsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <div className="h-40 my-2 flex justify-center items-center mb-1.5">
-        <h1 className="text-7xl font-bold">Rick and Morty </h1>
+        <h1 className="text-7xl font-bold max-[480px]:text-5xl">Rick and Morty </h1>
     </div>
   )
 }
