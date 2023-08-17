7918b19d17d1bde4bf4715cbed3a084255a954b3
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-17 13:55:51 +0100

    SearchBar UI fixed

--- RickandMortyTs/src/Components/Nav.tsx
+++ RickandMortyTs/src/Components/Nav.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar"
 
 const Nav = () => {
   return (
-    <div className=" h-24 flex justify-end ">
+    <div className=" h-24 flex justify-between items-center max-[480px] max-[480px]:justify-between">
        <SearchBar />
         <ul className="px-2 text-black flex h-12 items-center self-center text-xl max-[480px]:text-sm">
             <li className="mx-1"><a href="/characters">Characters</a></li>
--- RickandMortyTs/src/Components/SearchBar.tsx
+++ RickandMortyTs/src/Components/SearchBar.tsx
@@ -3,7 +3,9 @@
 const SearchBar = () => {
   return (
     <div>
-        <input/>
+        <input className="w-80 h-10 rounded-lg bg-gray-200 ml-20 mr-8 px-4 max-[480px]:w-20 max-[480px]:ml-3 max-[480px]:mr-3" 
+            placeholder="Search" type="text"/>
+        <button className="w-10 h-10 rounded-lg bg-green-200">Go</button>
     </div>
   )
 }
