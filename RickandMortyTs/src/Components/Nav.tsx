b4b476d389b2a6bde03f02627cdcf1b86c253185
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-17 10:46:08 +0100

    Nav: Making the lists links

--- RickandMortyTs/src/Components/Nav.tsx
+++ RickandMortyTs/src/Components/Nav.tsx
@@ -1,9 +1,10 @@
+
 const Nav = () => {
   return (
     <div className=" h-24 flex justify-end ">
         <ul className="px-2 text-black flex h-12 items-center self-center text-xl max-[480px]:text-sm">
-            <li className="mx-1">Characters</li>
-            <li className="mx-2">Episodes</li>
+            <li className="mx-1"><a href="#">Characters</a></li>
+            <li className="mx-2"><a href="#">Episodes</a></li>
         </ul>
     </div>
   )
