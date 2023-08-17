363cf88d2d143dc5a36cc469634f3f488f6dfe8e
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-17 13:19:11 +0100

    Browser Routing

--- RickandMortyTs/src/main.tsx
+++ RickandMortyTs/src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Character from './routes/Character.tsx'
+import Episodes from './routes/Episodes.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/characters",
+    element: <Character />
+  }, 
+  {
+    path: "episodes",
+    element:<Episodes />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    
+    <RouterProvider router={router} />
+    
   </React.StrictMode>,
 )
