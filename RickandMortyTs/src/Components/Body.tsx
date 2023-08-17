24e0fdbb39ad9112f02b52a4cdc521b01a9b57cc
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-17 10:46:52 +0100

    MainBody UI

--- RickandMortyTs/src/Components/Body.tsx
+++ RickandMortyTs/src/Components/Body.tsx
@@ -1,11 +1,12 @@
-import "./Style.css";
+
 
 const Body = () => {
   return (
     <div className="h-fit bg-zinc-800 py-6 flex justify-center">
       <div className="character-container h-max w-3/4 flex flex-row flex-wrap 
-          justify-between max-[480px]:bg-blue-400 max-[480px]:flex-col max-[480px]:justify-center">
-        <article className="bg-[#3C3E44] h-48 w-5/12 rounded-lg m-2 flex max-[800px]:h-fit max-[480px]:flex-col max-[480px]:w-9/12">
+          justify-between max-[480px]:bg-red-400 max-[480px]:flex-col  max-[480px]:justify-self-auto">
+        <article className="bg-[#3C3E44] h-48 w-5/12 rounded-lg m-2 flex max-[800px]:h-fit max-[480px]:flex-col 
+          max-[480px]:w-9/12 max-[480px]:justify-content-center">
           <div className="img-wrapper w-2/5 h-full max-[480px]:w-full">
             <img
               src="https://rickandmortyapi.com/api/character/avatar/38.jpeg"
@@ -34,17 +35,78 @@ const Body = () => {
             </div>
           </div>
         </article>
-        <article className="bg-[#3C3E44] h-48 w-5/12 rounded-lg m-2 flex">
-          <div className="img-wrapper w-2/5 h-full">
+        <article className="bg-[#3C3E44] h-48 w-5/12 rounded-lg m-2 flex max-[800px]:h-fit max-[480px]:flex-col 
+            max-[480px]:w-9/12 max-[480px]:justify-content-center">
+          <div className="img-wrapper w-2/5 h-full max-[480px]:w-full">
             <img
-              src="https://rickandmortyapi.com/api/character/avatar/135.jpeg"
-              className="h-full rounded-l-lg"
+              src="https://rickandmortyapi.com/api/character/avatar/38.jpeg"
+              alt="Beth"
+              className="h-full rounded-l-lg max-[480px]:rounded-r-lg "
             ></img>
           </div>
-          <div id="content-wrapper" className=" w-3/5 rounded-r-lg px-6 py-2">
+          <div id="content-wrapper" 
+            className=" w-3/5 rounded-r-lg px-6 py-2 max-[480px]:w-full">
             <div className="mb-2.5">
               <p className="text-zinc-100">Beth Smith</p>
-              <span className="text-sm text-zinc-100">
+              <span className="text-sm text-zinc-100 w-full">
+                <span id="status_icon"></span>
+                "Alive" "-" "Human"
+              </span>
+            </div>
+            <div className="mb-2.5">
+              <span className="text-sm text-gray-900">
+                Last known Location:
+              </span>
+              <p className="text-zinc-100">Earth(C-137)</p>
+            </div>
+            <div className="mb-2.5">
+              <span className="text-gray-900 text-sm">First seen in:</span>
+              <p className="text-zinc-100">Pilot</p>
+            </div>
+          </div>
+        </article>
+        <article className="bg-[#3C3E44] h-48 w-5/12 rounded-lg m-2 flex max-[800px]:h-fit max-[480px]:flex-col max-[480px]:w-9/12 max-[480px]:justify-content-center">
+          <div className="img-wrapper w-2/5 h-full max-[480px]:w-full">
+            <img
+              src="https://rickandmortyapi.com/api/character/avatar/38.jpeg"
+              alt="Beth"
+              className="h-full rounded-l-lg max-[480px]:rounded-r-lg "
+            ></img>
+          </div>
+          <div id="content-wrapper" 
+            className=" w-3/5 rounded-r-lg px-6 py-2 max-[480px]:w-full">
+            <div className="mb-2.5">
+              <p className="text-zinc-100">Beth Smith</p>
+              <span className="text-sm text-zinc-100 w-full">
+                <span id="status_icon"></span>
+                "Alive" "-" "Human"
+              </span>
+            </div>
+            <div className="mb-2.5">
+              <span className="text-sm text-gray-900">
+                Last known Location:
+              </span>
+              <p className="text-zinc-100">Earth(C-137)</p>
+            </div>
+            <div className="mb-2.5">
+              <span className="text-gray-900 text-sm">First seen in:</span>
+              <p className="text-zinc-100">Pilot</p>
+            </div>
+          </div>
+        </article>
+        <article className="bg-[#3C3E44] h-48 w-5/12 rounded-lg m-2 flex max-[800px]:h-fit max-[480px]:flex-col max-[480px]:w-9/12 max-[480px]:justify-content-center">
+          <div className="img-wrapper w-2/5 h-full max-[480px]:w-full">
+            <img
+              src="https://rickandmortyapi.com/api/character/avatar/38.jpeg"
+              alt="Beth"
+              className="h-full rounded-l-lg max-[480px]:rounded-r-lg "
+            ></img>
+          </div>
+          <div id="content-wrapper" 
+            className=" w-3/5 rounded-r-lg px-6 py-2 max-[480px]:w-full">
+            <div className="mb-2.5">
+              <p className="text-zinc-100">Beth Smith</p>
+              <span className="text-sm text-zinc-100 w-full">
                 <span id="status_icon"></span>
                 "Alive" "-" "Human"
               </span>
