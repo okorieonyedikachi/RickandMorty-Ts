55cdd90ed7b1b90f155aaac8362b92f546d2126b
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-08-17 13:21:50 +0100

    Pagination component

--- RickandMortyTs/src/App.tsx
+++ RickandMortyTs/src/App.tsx
@@ -2,6 +2,7 @@ import Nav from "./Components/Nav"
 import Header from "./Components/Header"
 import Body from "./Components/Body"
 import Footer from "./Components/Footer"
+import Pagination from "./Components/Pagination"
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
       <Nav />
       <Header />
       <Body /> 
+      <Pagination />
       <Footer />
     </>
   )
