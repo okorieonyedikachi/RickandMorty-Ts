42cac558639abfc8981623dff36d7aa7b3c65ad9
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-09-05 16:11:47 +0100

    Fix: Episode query and data mapping

--- RickandMortyTs/src/routes/Episodes.tsx
+++ RickandMortyTs/src/routes/Episodes.tsx
@@ -1,19 +1,22 @@
-// import Header from "../Components/Header"
-// import Body from "../Components/Body"
-
 import Header from "../Components/Header"
-// import { useQuery } from "@apollo/client"
-// import { LOAD_EPISODES } from "../Graphql/Queries";
+import { useQuery } from "@apollo/client"
+import { LOAD_EPISODES } from "../Graphql/Queries";
 import EpisodeBody from "../Components/EpisodeBody";
 import Pagination from "../Components/Pagination";
 import Footer from "../Components/Footer";
 
 const Episodes = () => {
-  // const {data } = useQuery(LOAD_EPISODES);
+  const {data } = useQuery(LOAD_EPISODES);
+  if (!data) {
+    return null
+  }
+  const episodeData = data?.episodes?.results; 
+  console.log(episodeData);
+  
   return (
     <>
         <Header />
-        <EpisodeBody />
+        <EpisodeBody data = {episodeData}/>
         <Pagination />
         <Footer />
     </>
